9dc6adb816b26c2fa5665c5b623475fdefd9e718
IdanGib <idangibly.dev@gmail.com>
2021-05-17 21:38:04 +0300

    remove apis

--- server/api/test.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = (router) => {
-    router.get('/', (req, res) => {
-        return res.send('test hidnsjkan');
-    });
-    return router;
-}
--- server/api/test2.js
+++
@@ -1,8 +0,0 @@
-const moment = require('moment');
-
-module.exports = (router) => {
-    router.get('/', (req, res) => {
-        return res.send('test2');
-    });
-    return router;
-}
